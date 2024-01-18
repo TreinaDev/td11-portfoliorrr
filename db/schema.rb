287e1a0a502b880cceb946550f01a33dbd66fa2e
Paulo Henrique <hreispaulo1@gmail.com>
2024-01-18 02:52:06 -0300

    Corrige ofensas rubocop
    Cria validação de unicidade para nome de categoria de trabalho no banco de dados
    
    Co-authored-by: Gabriel Manika <gmkoeb@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_17_135059) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_18_054350) do
   create_table "job_categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_job_categories_on_name", unique: true
   end
 
   create_table "posts", force: :cascade do |t|
