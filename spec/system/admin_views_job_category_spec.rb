33382ce5899205221084c8881ef2d7917d6d1f8b
Gabriel Manika Koeb <gmkoeb@gmail.com>
2024-01-18 10:44:37 -0300

    Protege link de criação de categorias de trabalho
    Garante que o link de criação de categorias de trabalho apareça
    somente para usuários administradores

---
+++ spec/system/admin_views_job_category_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'Usuário vê categorias de trabalho' do
+  it 'a partir da home' do
+    admin = create(:user, :admin)
+    create(:job_category, name: 'Web Design')
+    create(:job_category, name: 'Professor')
+
+    login_as admin
+    visit root_path
+    click_on 'Categorias de trabalho'
+
+    expect(page).to have_content 'Web Design'
+    expect(page).to have_content 'Professor'
+  end
+
+  it 'e não vê o link se não for administrador' do
+    user = create(:user)
+
+    login_as user
+    visit root_path
+
+    expect(page).not_to have_link 'Categorias de trabalho'
+  end
+end
