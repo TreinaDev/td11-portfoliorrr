da080e9a2b352129b9470daa88f6438e0291fd85
Rosemilson <rozbr96@gmail.com>
2024-01-24 17:50:33 -0300

    Adicionado feed de publicações
    Co-authored-by: Gabriel Manika <gmkoeb@users.noreply.github.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,11 @@
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @posts = Post.last(3)
+
+    return unless current_user
+
+    followed_users_id = Connection.where(follower: current_user.profile).active.pluck(:followed_profile_id)
+
+    @followed_posts = Post.where(user_id: followed_users_id).order(created_at: :desc)
+  end
 end
