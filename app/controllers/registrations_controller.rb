64ae653b2631fafc0a1a490d1a01801af2367eeb
Danilo Martins <dansm86@outlook.com>
2024-01-25 12:18:03 -0300

    fix: rubocop erros
    - adiciona link para voltar na página de edição de dados pessoais
    - adicionar resources como parâmetro na callbak after_sign_up_for
    
    Co-authored-by: ana resgalla <anaresgalla@users.noreply.github.com>

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    edit_user_profile_path
+    edit_user_profile_path if resource.role == 'user'
   end
 end
