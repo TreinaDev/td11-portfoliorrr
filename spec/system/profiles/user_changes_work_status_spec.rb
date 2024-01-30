e5ec39e94c933110abbd9aa0816c5cc6eb1aceda
Eliseu Ramos <eliseu.mec@gmail.com>
2024-01-30 13:38:17 -0300

    Adiciona testes de proteção de rota
    Co-authored-by: ana resgalla <anaresgalla@users.noreply.github.com>

--- spec/system/profiles/user_changes_work_status_spec.rb
+++ spec/system/profiles/user_changes_work_status_spec.rb
@@ -1,30 +1,57 @@
 require 'rails_helper'
 
 describe 'Usuário altera o status de disponibilidade de trabalho' do
-  it 'de disponível para indisponível' do
-    user = create(:user)
+  context 'de disponível para indisponível' do
+    it 'com sucesso' do
+      user = create(:user)
 
-    login_as user
-    visit profile_path(user.profile)
-    click_on 'Não estou disponível para trabalho'
+      login_as user
+      visit profile_path(user.profile)
+      click_on 'Não estou disponível para trabalho'
 
-    expect(page).to have_current_path profile_path(user.profile)
-    expect(page).to have_content 'Alteração salva com sucesso'
-    expect(page).not_to have_button 'Não estou disponível para trabalho'
-    expect(page).to have_content 'Indisponível para trabalho'
+      expect(page).to have_current_path profile_path(user.profile)
+      expect(page).to have_content 'Alteração salva com sucesso'
+      expect(page).not_to have_button 'Não estou disponível para trabalho'
+      expect(page).to have_content 'Indisponível para trabalho'
+    end
+
+    it 'apenas do seu próprio perfil' do
+      user_a = create(:user)
+      user_b = create(:user)
+
+      login_as user_b
+      visit profile_path(user_a.profile)
+
+      expect(page).to have_content 'Disponível para trabalho'
+      expect(page).not_to have_button 'Não estou disponível para trabalho'
+    end
   end
 
-  it 'de indisponível para disponível' do
-    user = create(:user)
-    user.profile.unavailable!
+  context 'de indisponível para disponível' do
+    it 'com sucesso' do
+      user = create(:user)
+      user.profile.unavailable!
 
-    login_as user
-    visit profile_path(user.profile)
-    click_on 'Estou disponível para trabalho'
+      login_as user
+      visit profile_path(user.profile)
+      click_on 'Estou disponível para trabalho'
 
-    expect(page).to have_current_path profile_path(user.profile)
-    expect(page).to have_content 'Alteração salva com sucesso'
-    expect(page).not_to have_button 'Estou disponível para trabalho'
-    expect(page).to have_content 'Disponível para trabalho'
+      expect(page).to have_current_path profile_path(user.profile)
+      expect(page).to have_content 'Alteração salva com sucesso'
+      expect(page).not_to have_button 'Estou disponível para trabalho'
+      expect(page).to have_content 'Disponível para trabalho'
+    end
+
+    it 'apenas do seu próprio perfil' do
+      user_a = create(:user)
+      user_b = create(:user)
+      user_a.profile.unavailable!
+
+      login_as user_b
+      visit profile_path(user_a.profile)
+
+      expect(page).to have_content 'Indisponível para trabalho'
+      expect(page).not_to have_button 'Estou disponível para trabalho'
+    end
   end
 end
