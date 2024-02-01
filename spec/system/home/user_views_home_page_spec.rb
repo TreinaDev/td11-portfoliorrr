6e39d24c045f4d9ecb06b642f89f6c9de41d8e5a
Paulo Henrique <hreispaulo1@gmail.com>
2024-02-01 14:22:51 -0300

    Test/Visitante não vê mais seguidos na home
    Co-authored-by: Rosemilson <rozbr96@gmail.com>

--- spec/system/home/user_views_home_page_spec.rb
+++ spec/system/home/user_views_home_page_spec.rb
@@ -125,5 +125,32 @@ describe 'Usuário visita a home page' do
       expect(page).not_to have_content 'Como fazer uma app Vue'
       expect(page).not_to have_content 'Como fazer uma app React'
     end
+
+    it 'e não vê usuários mais seguidos' do
+      first_user = create(:user)
+      second_user = create(:user)
+      third_user = create(:user)
+      fourth_user = create(:user)
+      fifth_user = create(:user)
+
+      Connection.create!(follower: fourth_user.profile, followed_profile: third_user.profile)
+      Connection.create!(follower: fifth_user.profile, followed_profile: third_user.profile)
+
+      Connection.create!(follower: second_user.profile, followed_profile: first_user.profile)
+      Connection.create!(follower: third_user.profile, followed_profile: first_user.profile)
+      Connection.create!(follower: fourth_user.profile, followed_profile: first_user.profile)
+      Connection.create!(follower: fifth_user.profile, followed_profile: first_user.profile)
+
+      Connection.create!(follower: third_user.profile, followed_profile: second_user.profile)
+      Connection.create!(follower: fourth_user.profile, followed_profile: second_user.profile)
+      Connection.create!(follower: fifth_user.profile, followed_profile: second_user.profile)
+
+      visit root_path
+
+      expect(page).not_to have_content 'Usuários mais seguidos'
+      expect(page).not_to have_content first_user.full_name
+      expect(page).not_to have_content second_user.full_name
+      expect(page).not_to have_content third_user.full_name
+    end
   end
 end
