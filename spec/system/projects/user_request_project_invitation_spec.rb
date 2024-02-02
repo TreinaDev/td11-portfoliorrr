feeec5b9f5f379f9b7810d03a3882ca4fa7fd9e1
Danilo Martins <dansm86@outlook.com>
2024-02-02 12:04:24 -0300

    fix/test/adiciona teste envio sem mensagem
    - solicitante do convite não precisa passar mensagem na requisição
    
    Co-authored-by: Gabriel Manika <gmkoeb@users.noreply.github.com>

--- spec/system/projects/user_request_project_invitation_spec.rb
+++ spec/system/projects/user_request_project_invitation_spec.rb
@@ -24,5 +24,27 @@ describe 'Usuário solicita convite para projetos' do
       expect(page).to have_button 'Convite Solicitado', disabled: true
       expect(page).to have_button 'Solicitar Convite', count: 2
     end
+
+    it 'e não é necessário passar uma mensagem' do
+      json_projects_data = File.read(Rails.root.join('./spec/support/json/projects.json'))
+
+      fake_projects_response = double('faraday_response', status: 200, body: json_projects_data)
+
+      allow(Faraday).to receive(:get).with('http://localhost:4000/api/v1/projects').and_return(fake_projects_response)
+
+      user = create(:user)
+
+      login_as user
+
+      visit projects_path
+
+      click_on 'Solicitar Convite', match: :first
+      click_button 'Enviar'
+
+      expect(page).to have_content 'Solicitação enviada com sucesso!'
+      expect(user.profile.invitation_requests.count).to eq 1
+      expect(page).to have_button 'Convite Solicitado', disabled: true
+      expect(page).to have_button 'Solicitar Convite', count: 2
+    end
   end
 end
