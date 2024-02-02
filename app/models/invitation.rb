6c15ad4b13da3166516b1cd2c4ddf1c49a8468ff
Rosemilson <rozbr96@gmail.com>
2024-02-02 10:07:19 -0300

    Adiciona testes de validação para os invitates
    Co-authored-by: Paulo Henrique <hreis1@users.noreply.github.com>

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,5 +1,8 @@
 class Invitation < ApplicationRecord
   belongs_to :profile
 
+  validates :profile_id, :project_title, :project_description,
+            :project_category, :colabora_invitation_id, presence: true
+
   enum status: { pending: 0, accepted: 1, declined: 2, cancelled: 3, expired: 4, removed: 5 }
 end
