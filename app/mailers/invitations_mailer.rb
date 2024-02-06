581cee774bc15f06b868b5bafc8839b11c81ef2e
anaresgalla <anaresgalla@gmail.com>
2024-02-06 17:46:44 -0300

    Adiciona tradução
    Co-authored-by: Paulo Henrique <hreis1@users.noreply.github.com>

--- app/mailers/invitations_mailer.rb
+++ app/mailers/invitations_mailer.rb
@@ -1,8 +1,8 @@
 class InvitationsMailer < ApplicationMailer
-
   def received_invitation
     profile = Profile.find(params[:profile_id])
     project_title = params[:project_title]
-    mail(subject: 'Você recebeu um convite', to: profile.user.email, body: "Você recebeu um convite para participar do projeto #{project_title}.")
+    mail(subject: t('.subject'), to: profile.user.email,
+         body: "Você recebeu um convite para participar do projeto #{project_title}.")
   end
 end
