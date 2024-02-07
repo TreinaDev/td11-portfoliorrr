495bda93659072ee41553febc5a8856b0eef1dcc
Rosemilson <rozbr96@gmail.com>
2024-02-07 17:19:47 -0300

    Adiciona tratamento de erro dentro do job
    Co-authored-by: Lucas Vasques <lucasvc1996@gmail.com>

--- app/jobs/decline_invitation_job.rb
+++ app/jobs/decline_invitation_job.rb
@@ -1,9 +1,10 @@
 class DeclineInvitationJob < ApplicationJob
-  retry_on Faraday::ConnectionFailed, attempts: 5
-  retry_on Faraday::ServerError
+  retry_on Faraday::ConnectionFailed, Faraday::ServerError, wait: :polynomially_longer, attempts: 5 do |job|
+    job.arguments.first.pending!
+  end
 
   def perform(invitation)
-    url = "http://localhost:5000/api/v1/invitations/#{invitation.colabora_invitation_id}"
+    url = "http://localhost:4000/api/v1/invitations/#{invitation.colabora_invitation_id}"
     Faraday.new { |faraday| faraday.response :raise_error }.patch(url)
     invitation.declined!
   rescue Faraday::ResourceNotFound, Faraday::ConflictError
