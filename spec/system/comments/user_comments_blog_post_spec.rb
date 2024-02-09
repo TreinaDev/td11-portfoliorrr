0cb02df03e13990c6f76674d582c1395a1ae06f7
Gabriel Manika Koeb <gmkoeb@gmail.com>
2024-02-09 12:58:19 -0300

    Feat/Adiciona link para perfis de autores de comentarios e posts

--- spec/system/comments/user_comments_blog_post_spec.rb
+++ spec/system/comments/user_comments_blog_post_spec.rb
@@ -12,7 +12,7 @@ describe 'Usuário comenta uma publicação' do
       click_on 'Comentar'
 
       within '#comments' do
-        expect(page).to have_content 'Peter Parker'
+        expect(page).to have_link 'Peter Parker', href: profile_path(commenter.profile)
         expect(page).to have_content 'Um comentário legal'
       end
       expect(post).to be_persisted
@@ -27,7 +27,7 @@ describe 'Usuário comenta uma publicação' do
       click_on 'Comentar'
 
       within '#comments' do
-        expect(page).to have_content "#{post.user.full_name} (autor)"
+        expect(page).to have_link "#{post.user.full_name} (autor)", href: profile_path(post.user.profile)
         expect(page).to have_content 'Meu post é fenomenal, 10/10, magnum opus.'
       end
       expect(post).to be_persisted
