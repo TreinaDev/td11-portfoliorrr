e79707a8cb2ad24a2e933d8e12d3d9cfa5434ffe
Paulo Henrique <hreispaulo1@gmail.com>
2024-02-09 10:58:10 -0300

    test/adiciona testes
    -Notificação de novo seguidor
    -Não notifica quem não é seguidor
    
    Co-authored-by: ana resgalla <anaresgalla@users.noreply.github.com>

---
+++ spec/system/notifications/user_sees_new_follower_notification_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'Usuário vê notificação de novo seguidor' do
+  it 'com sucesso' do
+    follower = create(:user, full_name: 'Paulo')
+    followed = create(:user, full_name: 'Ana')
+    Connection.create!(followed_profile: followed.profile, follower: follower.profile)
+
+    login_as followed
+    visit root_path
+    click_button class: 'dropdown-toggle'
+    within 'nav' do
+      click_on 'Notificações'
+    end
+
+    expect(page).to have_current_path notifications_path
+    expect(page).to have_content "#{follower.full_name} começou a te seguir"
+    expect(page).to have_link "#{follower.full_name}", href: profile_path(follower)
+  end
+end
--- spec/system/notifications/user_sees_post_notification_spec.rb
+++ spec/system/notifications/user_sees_post_notification_spec.rb
@@ -18,4 +18,24 @@ describe 'Nova publicação envia notificação para seguidores' do
     expect(page).to have_content "#{followed.full_name} fez uma publicação"
     expect(page).to have_link "publicação", href: post_path(post)
   end
+  
+  it 'e nao envia para quem nao segue' do
+    paulo = create(:user, full_name: 'Paulo')
+    followed_by_paulo = create(:user, full_name: 'Ana')
+    Connection.create!(followed_profile: followed_by_paulo.profile, follower: paulo.profile)
+    other_user = create(:user, full_name: 'João')
+    post = create(:post, user: followed_by_paulo)
+
+    login_as other_user
+    visit root_path
+    click_button class: 'dropdown-toggle'
+    within 'nav' do
+      click_on 'Notificações'
+    end
+
+    expect(page).to have_current_path notifications_path
+    expect(page).not_to have_content "#{followed_by_paulo.full_name} fez uma publicação"
+    expect(other_user.profile.notifications).to be_empty
+    expect(paulo.profile.notifications.count).to eq 1
+  end
 end
