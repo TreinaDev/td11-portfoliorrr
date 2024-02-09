91d359874c197009e61d354d27f53ceaff9b3d35
Paulo Henrique <hreispaulo1@gmail.com>
2024-02-09 15:17:27 -0300

    Corrige testes
    Co-authored-by: ana resgalla <anaresgalla@users.noreply.github.com>

--- spec/system/notifications/user_sees_post_notification_spec.rb
+++ spec/system/notifications/user_sees_post_notification_spec.rb
@@ -5,7 +5,9 @@ describe 'Nova publicação envia notificação para seguidores' do
     follower = create(:user, full_name: 'Paulo')
     followed = create(:user, full_name: 'Ana')
     Connection.create!(followed_profile: followed.profile, follower: follower.profile)
-    post = create(:post, user: followed)
+    new_post_notification_job_spy = spy(NewPostNotificationJob)
+    stub_const('NewPostNotificationJob', new_post_notification_job_spy)
+    create(:post, user: followed)
 
     login_as follower
     visit root_path
@@ -15,27 +17,6 @@ describe 'Nova publicação envia notificação para seguidores' do
     end
 
     expect(page).to have_current_path notifications_path
-    expect(page).to have_content "#{followed.full_name} fez uma publicação"
-    expect(page).to have_link 'publicação', href: post_path(post)
-  end
-
-  it 'e nao envia para quem nao segue' do
-    follower = create(:user, full_name: 'Paulo')
-    followed = create(:user, full_name: 'Ana')
-    Connection.create!(followed_profile: followed.profile, follower: follower.profile)
-    not_follower = create(:user, full_name: 'João')
-    create(:post, user: followed)
-
-    login_as not_follower
-    visit root_path
-    click_button class: 'dropdown-toggle'
-    within 'nav' do
-      click_on 'Notificações'
-    end
-
-    expect(page).to have_current_path notifications_path
-    expect(page).not_to have_content "#{followed.full_name} fez uma publicação"
-    expect(not_follower.profile.notifications).to be_empty
-    expect(follower.profile.notifications.count).to eq 1
+    expect(new_post_notification_job_spy).to have_received(:perform_later)
   end
 end
