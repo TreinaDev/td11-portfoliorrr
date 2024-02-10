3f7c73623a91a1b253d9205768bbc9f9f45eba7e
Paulo Henrique <hreispaulo1@gmail.com>
2024-02-09 23:17:09 -0300

    remove não usada

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,10 +4,4 @@ class NotificationsController < ApplicationController
   def index
     @notifications = current_user.profile.notifications.order(created_at: :desc)
   end
-
-  def mark_as_read
-    @notification = Notification.find(params[:id])
-    @notification.update(read_at: Time.zone.now)
-    redirect_to request.referer
-  end
 end
