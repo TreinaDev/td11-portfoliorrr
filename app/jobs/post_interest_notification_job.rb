c17f93e22eb7814b45d435fd292042fd2abfa697
Danilo Martins <dansm86@outlook.com>
2024-02-10 18:24:53 -0300

    wip/user vê notificações de posts de interesse
    - setado números de threads do solid queue para 1 devido sqlite não suportar multithread
    - adicionado views para notificações na pagina de notificações
    - craido testes para view de notificações
    
    Co-authored-by: Paulo Henrique Meneses <paulohenrique-gh@users.noreply.github.com>

--- app/jobs/post_interest_notification_job.rb
+++ app/jobs/post_interest_notification_job.rb
@@ -2,11 +2,11 @@ class PostInterestNotificationJob < ApplicationJob
   queue_as :default
 
   def perform(comment)
+    Rails.logger.debug '------------------------------------'
+    Rails.logger.debug 'Iniciando o Job de post de interesse'
     post = comment.post
     users = post.comments.map(&:user).uniq.excluding(comment.user, post.user)
-
-    users.each do |user|
-      Notification.create!(profile: user.profile, notifiable: comment)
-    end
+    users.each { |user| Notification.create!(profile: user.profile, notifiable: comment) }
+    Rails.logger.debug 'Finalizando o Job de post de interesse'
   end
 end
