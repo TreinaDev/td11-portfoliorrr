415d4668a245e90d6d149d4b5eed50213d1f1bef
Danilo Martins <dansm86@outlook.com>
2024-02-12 14:43:32 -0300

    chore/removed rails logger from job
    Co-authored-by: Paulo Henrique Meneses <ph.1928@hotmail.com>

--- app/jobs/post_interest_notification_job.rb
+++ app/jobs/post_interest_notification_job.rb
@@ -2,11 +2,8 @@ class PostInterestNotificationJob < ApplicationJob
   queue_as :default
 
   def perform(comment)
-    Rails.logger.debug '------------------------------------'
-    Rails.logger.debug 'Iniciando o Job de post de interesse'
     post = comment.post
     users = post.comments.map(&:user).uniq.excluding(comment.user, post.user)
     users.each { |user| Notification.create!(profile: user.profile, notifiable: comment) }
-    Rails.logger.debug 'Finalizando o Job de post de interesse'
   end
 end
