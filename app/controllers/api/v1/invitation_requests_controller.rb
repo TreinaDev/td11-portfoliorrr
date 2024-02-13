cc63258c6047c932ec194c9bf938d34ddaabe22d
Eliseu Ramos <eliseu.mec@gmail.com>
2024-02-13 14:41:11 -0300

    Adiciona regra para validar status da solicitação de convite
    Retorna status 409 para requisições em solicitações de convite que não estejam pendentes.
    
    Co-authored-by: Rodrigo Gyodai <GyodaiDDA@users.noreply.github.com>

--- app/controllers/api/v1/invitation_requests_controller.rb
+++ app/controllers/api/v1/invitation_requests_controller.rb
@@ -3,8 +3,12 @@ module Api
     class InvitationRequestsController < ApiController
       def update
         invitation_request = InvitationRequest.find(params[:id])
-        invitation_request.refused!
-        render status: :ok, json: invitation_request.as_json(except: %i[created_at updated_at])
+        if invitation_request.pending?
+          invitation_request.refused!
+          render status: :ok, json: invitation_request.as_json(except: %i[created_at updated_at])
+        else
+          render status: :conflict, json: { errors: I18n.t('.api_not_pending_error') }
+        end
       end
     end
   end
--- spec/requests/apis/v1/invitation_requests/leader_denies_invitation_request_spec.rb
+++ spec/requests/apis/v1/invitation_requests/leader_denies_invitation_request_spec.rb
@@ -21,7 +21,40 @@ describe 'API solicitações' do
       expect(json_response['status']).to eq 'refused'
     end
 
-    pending 'retorna erro 409 se a solicitação já estiver recusada'
+    it 'retorna erro 409 se a solicitação não estiver pendente' do
+      request1 = create(:invitation_request, status: :processing)
+      request2 = create(:invitation_request, status: :accepted)
+      request3 = create(:invitation_request, status: :refused)
+      request4 = create(:invitation_request, status: :error)
+      request5 = create(:invitation_request, status: :aborted)
+
+      response_status1 = patch "/api/v1/invitation_requests/#{request1.id}"
+      response_status2 = patch "/api/v1/invitation_requests/#{request2.id}"
+      response_status3 = patch "/api/v1/invitation_requests/#{request3.id}"
+      response_status4 = patch "/api/v1/invitation_requests/#{request4.id}"
+      response_status5 = patch "/api/v1/invitation_requests/#{request5.id}"
+      request1.reload
+      request2.reload
+      request3.reload
+      request4.reload
+      request5.reload
+
+      expect(response_status1).to eq 409
+      expect(response_status2).to eq 409
+      expect(response_status3).to eq 409
+      expect(response_status4).to eq 409
+      expect(response_status5).to eq 409
+      expect(request1).to be_processing
+      expect(request2).to be_accepted
+      expect(request3).to be_refused
+      expect(request4).to be_error
+      expect(request5).to be_aborted
+      expect(response.content_type).to include 'application/json'
+      json_last_response = JSON.parse(response.body)
+      expect(json_last_response.class).to eq Hash
+      expect(json_last_response['errors']).to include 'Solicitação de convite não está pendente para ser recusada.'
+    end
+
     pending 'retorna erro 404 se a solicitação não for encontrada'
     pending 'retorna erro 400 se a requisição não contiver um id de solicitação de convite'
     pending 'retorna erro 500, com mensagem identificando erro do servidor'
