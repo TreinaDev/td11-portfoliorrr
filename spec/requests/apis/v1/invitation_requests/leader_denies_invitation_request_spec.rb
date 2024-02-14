c873b86753677753e042fdf97d7a00ba57e16a05
Eliseu Ramos <eliseu.mec@gmail.com>
2024-02-14 10:30:28 -0300

    Altera teste para garantir que ele é auto-contido
    Co-authored-by: rodrigo.gyodai@gmail.com <rodrigo.gyodai@gmail.com>

--- spec/requests/apis/v1/invitation_requests/leader_denies_invitation_request_spec.rb
+++ spec/requests/apis/v1/invitation_requests/leader_denies_invitation_request_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 describe 'API solicitações' do
   context 'PATCH /api/v1/invitation_requests/:id' do
     it 'altera o status para recusado com sucesso' do
-      invitation_request = create(:invitation_request, status: :pending)
+      user = create(:user)
+      invitation_request = create(:invitation_request, profile: user.profile, status: :pending, message: 'oi projeto')
 
       patch "/api/v1/invitation_requests/#{invitation_request.id}"
       invitation_request.reload
@@ -12,10 +13,10 @@ describe 'API solicitações' do
       expect(response.content_type).to include('application/json')
       json_response = JSON.parse(response.body)
       expect(json_response.class).to eq Hash
-      expect(json_response['id']).to eq invitation_request.id
-      expect(json_response['profile_id']).to eq invitation_request.profile.id
-      expect(json_response['message']).to eq invitation_request.message
-      expect(json_response['project_id']).to eq invitation_request.project_id
+      expect(json_response['id']).to eq 1
+      expect(json_response['profile_id']).to eq 1
+      expect(json_response['message']).to eq 'oi projeto'
+      expect(json_response['project_id']).to eq 1
       expect(json_response.keys).not_to include 'created_at'
       expect(json_response.keys).not_to include 'updated_at'
       expect(json_response['status']).to eq 'refused'
