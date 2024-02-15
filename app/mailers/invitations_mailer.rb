064964448ab0082aa2d422823d3e556defac54de
anaresgalla <anaresgalla@gmail.com>
2024-02-15 19:39:31 -0300

    Corrige testes quebrados e problemas de rubocop

--- app/mailers/invitations_mailer.rb
+++ app/mailers/invitations_mailer.rb
@@ -1,7 +1,7 @@
 class InvitationsMailer < ApplicationMailer
   def received_invitation
-    profile = Profile.friendly.find(params[:profile_id])
-    project_title = params[:project_title]
-    mail(subject: t('.subject'), to: profile.user.email)
+    @profile = Profile.friendly.find(params[:profile_id])
+    @project_title = params[:project_title]
+    mail(subject: t('.subject'), to: @profile.user.email)
   end
 end
