d0fbb09d5636ea777bad93257c4f7d5e6dd028d8
Gabriel Manika Koeb <gmkoeb@gmail.com>
2024-02-15 10:48:01 -0300

    Style/melhora layout e adiciona botão de compartilhar nos posts
    Co-authored-by: GyodaiDDA <rodrigo.gyodai@gmail.com>

--- spec/system/posts/user_edits_post_status_spec.rb
+++ spec/system/posts/user_edits_post_status_spec.rb
@@ -23,6 +23,7 @@ describe 'Usuário edita status da publicação' do
 
     expect(page).to have_current_path post_path(post)
     expect(Post.last.status).to eq 'draft'
+    expect(page).to have_content('Rascunho')
   end
 
   it 'publica rascunho' do
@@ -35,5 +36,6 @@ describe 'Usuário edita status da publicação' do
 
     expect(page).to have_current_path post_path(post)
     expect(Post.last.status).to eq 'published'
+    expect(page).to have_content('Publicada')
   end
 end
