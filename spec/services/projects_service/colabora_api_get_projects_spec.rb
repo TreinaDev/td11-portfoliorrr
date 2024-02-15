c3a3d603594554ecd1b8ec9b306fc8c953e34a61
Eliseu Ramos <eliseu.mec@gmail.com>
2024-02-15 13:46:50 -0300

    Corrige problemas de lint (rubocop)

--- spec/services/projects_service/colabora_api_get_projects_spec.rb
+++ spec/services/projects_service/colabora_api_get_projects_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe ProjectsService::ColaBoraApiGetProjects do
     it 'retorna array de objetos do tipo Project' do
       json_data = File.read(Rails.root.join('./spec/support/json/projects.json'))
       fake_response = double('faraday_response', success?: true, body: json_data)
-      
+
       allow(Faraday).to receive(:get).with('http://localhost:3000/api/v1/projects').and_return(fake_response)
-      
+
       result = ProjectsService::ColaBoraApiGetProjects.send
 
       json_response = JSON.parse(result.body)
