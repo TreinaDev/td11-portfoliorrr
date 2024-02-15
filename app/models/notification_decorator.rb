90b1c36b6fdb4b6afab349c5f884ab402165f764
Paulo Henrique <hreispaulo1@gmail.com>
2024-02-15 13:11:32 -0300

    refactor/renomeia método
    -redirect_path -> redirect_after_click

--- app/models/notification_decorator.rb
+++ app/models/notification_decorator.rb
@@ -1,14 +1,14 @@
 class NotificationDecorator < SimpleDelegator
-  def redirect_path
+  def redirect_after_click
     case notifiable
     when Comment
       notifiable.post
     when Connection
       notifiable.follower
-    when Post, Invitation
-      notifiable
-    else
+    when Like
       notifiable.likeable.is_a?(Comment) ? notifiable.likeable.post : notifiable.likeable
+    else
+      notifiable
     end
   end
 end
