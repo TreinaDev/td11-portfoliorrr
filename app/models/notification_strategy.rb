c9fb2532a5c0346c4b3e7e400b824af6cb393c7f
Paulo Henrique <hreispaulo1@gmail.com>
2024-02-16 02:10:37 -0300

    refatora NotificationStrategy

--- app/models/notification_strategy.rb
+++ app/models/notification_strategy.rb
@@ -1,14 +1,13 @@
 class NotificationStrategy < SimpleDelegator
+  STRATEGY = {
+    Invitation => ->(notifiable) { notifiable },
+    Post => ->(notifiable) { notifiable },
+    Comment => ->(notifiable) { notifiable.post },
+    Connection => ->(notifiable) { notifiable.follower },
+    Like => ->(notifiable) { notifiable.likeable.is_a?(Comment) ? notifiable.likeable.post : notifiable.likeable }
+  }.freeze
+
   def redirect_after_click
-    case notifiable
-    when Comment
-      notifiable.post
-    when Connection
-      notifiable.follower
-    when Like
-      notifiable.likeable.is_a?(Comment) ? notifiable.likeable.post : notifiable.likeable
-    else
-      notifiable
-    end
+    STRATEGY[notifiable.class].call(notifiable)
   end
 end
